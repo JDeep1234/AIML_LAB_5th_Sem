8afc5c6ca4e5e7774386e628e11e1bfcb9940b61
JDeep <132117873+JDeep1234@users.noreply.github.com>
2025-02-04 07:45:08 +0530

    Update tictactoe.py

--- tictactoe.py
+++ tictactoe.py
@@ -1,7 +1,8 @@
 class TicTacToe:
     def __init__(self): self.b, self.p = [[' ']*3 for _ in range(3)], 'X'
     def p_b(self): print('\n'.join([' | '.join(r) for r in self.b])+'\n')
-    def g_o(self): return any([self.b[i][0]==self.b[i][1]==self.b[i][2]!=' ' for i in range(3)] + [self.b[0][i]==self.b[1][i]==self.b[2][i]!=' ' for i in range(3)] + [self.b[0][0]==self.b[1][1]==self.b[2][2]!=' ', self.b[0][2]==self.b[1][1]==self.b[2][0]!=' '])
+    def g_o(self): 
+        return any([self.b[i][0]==self.b[i][1]==self.b[i][2]!=' ' for i in range(3)] + [self.b[0][i]==self.b[1][i]==self.b[2][i]!=' ' for i in range(3)] + [self.b[0][0]==self.b[1][1]==self.b[2][2]!=' ', self.b[0][2]==self.b[1][1]==self.b[2][0]!=' '])
     def draw(self): return all(c!=' ' for r in self.b for c in r)
     def dfs(self, p):
         if (w:=self.g_o()): return {'s': 1 if self.p=='X' else -1}
