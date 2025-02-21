d167525227889b3783a50d1e26f3e7670c0b1353
JDeep <132117873+JDeep1234@users.noreply.github.com>
2025-02-21 18:01:45 +0530

    Update knn.py

--- knn.py
+++ knn.py
@@ -9,4 +9,8 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_
 predict = lambda X: np.array([Counter(y_train[np.argsort([np.linalg.norm(x - x_train) for x_train in X_train])[:3]]).most_common(1)[0][0] for x in X])
 y_pred = predict(X_test)
 
-print(f'Accuracy: {np.mean(y_pred == y_test):.4f}', "\nPredictions:", names[y_pred], "\nConfusion Matrix:\n", confusion_matrix(y_test, y_pred), "\nClassification Report:\n", classification_report(y_test, y_pred, target_names=names))
+print(f'Accuracy: {np.mean(y_pred == y_test):.4f}',
+      "\nPredictions:", names[y_pred], "\nConfusion Matrix:\n", 
+      confusion_matrix(y_test, y_pred), 
+      "\nClassification Report:\n", 
+      classification_report(y_test, y_pred, target_names=names))
